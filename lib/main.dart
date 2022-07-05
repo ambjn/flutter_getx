a964773fb5169be04265ea736cdd11a65da1b4a3
Amber Jain <jainanuamber@gmail.com>
2022-07-05 23:53:00 +0530

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_getx/my_home_page.dart';
 
 void main() => runApp(const MyApp());
 
@@ -8,10 +9,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter GetX',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
+      home: MyHomePage(),
     );
   }
 }
